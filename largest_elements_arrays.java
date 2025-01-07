81237a68d634c8d57d4f43a8fee33049c5783def
ali-sorathiya64 <alisorathiya970@gmail.com>
2025-01-07 10:47:47 -0800

    add

--- largest_elements_arrays.java
+++ largest_elements_arrays.java
@@ -1,21 +1,28 @@
 public class largest_elements_arrays {
 
-    public static int largest_value(int numbers[])
+    public static int largest_value_smallest_value(int numbers[])
    {
     int largest =Integer.MIN_VALUE;
+    int smallest =Integer.MAX_VALUE;
      for (int i=0; i<numbers.length;i++){
         if(largest < numbers[i]){
             largest =numbers[i];
         }
+        if (smallest> numbers[i]){
+            smallest =numbers[i];
+        }
         
-    
+        
      } 
+     System.out.println("Smallest element of an array is :"+smallest);
+   System.out.println("Largest element of an array is :"+largest);
      return largest;
     
    }
     public static void main(String args[]){
         int number[]={5,4,8,7,6};
-        System.out.println("Largest value of an array is :"+largest_value(number));
+    int value = largest_value_smallest_value(number);
+        
 
     }
 }
---
+++ leetcode_9.java
@@ -0,0 +1,34 @@
+public class leetcode_9 {
+
+    public static boolean palindrome_number(int x){
+        
+        while (x<0){
+      return false;
+            
+        }
+        int n =x;
+        int rev=0;
+while (n>0) {
+    
+
+        int last_digit = n%10;
+        rev =(rev*10)+last_digit;
+        n=n/10;
+}
+        if (rev==x){
+             System.out.println("It's is a palindrome number");
+            return true;
+        }
+        else{
+             System.out.println("It's is not an palindrome number");
+            return false;
+        }
+    
+
+    }
+    public static void main(String args[]){
+        palindrome_number(111);
+
+
+    }
+}
